e3e86710579ff348df9f10ad93611c845e946055
LAPTOP-5B9CGFQ3\soo98 <https://github.com/hansupark/phs123.github.io.git>
2018-12-10 23:20:39 +0900

    notifier + command

---
+++ HairSalonManager/ViewModel/Notifier.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+
+public class Notifier : INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler PropertyChanged; /*프로퍼티가 바뀌는걸 감지 --> 새롭게 업데이트 시켜주는 클래스*/
+
+    protected void OnPropertyChanged(string propertyName)
+    {
+        if (PropertyChanged != null)
+        {
+            PropertyChanged(this,
+                new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
