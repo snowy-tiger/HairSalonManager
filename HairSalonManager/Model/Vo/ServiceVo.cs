6218bbef9219ef18f417f20aefbf90d765354378
LAPTOP-5B9CGFQ3\soo98 <https://github.com/hansupark/phs123.github.io.git>
2018-12-10 21:50:03 +0900

    vo timeTable까지

---
+++ HairSalonManager/Model/Vo/ServiceVo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HairSalonManager.Model.Vo
+{
+    class ServiceVo
+    {
+        public int ServiceId { get; set; }
+
+        public string ServiceName { get; set; }
+
+        public int ServicePrice { get; set; }
+
+        public int ServiceTime { get; set; }
+
+        public string ServiceDescription { get; set; }
+    }
+}
