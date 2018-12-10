eca4ee67788e7fe43fc9501ce8cfb72e9cec468a
HyeonBeom <hyunbum75@gmail.com>
2018-12-11 00:02:19 +0900

    Add Minimize Action

--- HairSalonManager/ViewModel/MainWindowViewModel.cs
+++ HairSalonManager/ViewModel/MainWindowViewModel.cs
@@ -3,18 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace HairSalonManager.ViewModel
 {
-    class MainWindowViewModel
+    class MainWindowViewModel:Notifier
     {
+        private WindowState _windowState;
+
+        public WindowState WindowState
+        {
+            get { return _windowState; }
+            set
+            {
+                _windowState = value;
+                OnPropertyChanged("WindowState");
+            }
+        }
+
+
+        public Command MinimizeCommand { get; set; }
         public Command CloseCommand { get; set; }
 
         public MainWindowViewModel()
         {
+            MinimizeCommand = new Command(MinimizeMethod, CanExecuteMethod);
             CloseCommand = new Command(CloseMethod, CanExecuteMethod);
         }
 
+        private void MinimizeMethod(object parameter)
+        {
+            WindowState = WindowState.Minimized;
+        }
         private void CloseMethod(object parameter)
         {
             Environment.Exit(0);
