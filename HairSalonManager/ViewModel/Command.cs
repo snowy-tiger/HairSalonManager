735add6a9c0f3f7e8aa555c15c0ebc0fb9054948
LAPTOP-5B9CGFQ3\soo98 <https://github.com/hansupark/phs123.github.io.git>
2018-12-10 22:56:12 +0900

    command

---
+++ HairSalonManager/ViewModel/Command.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace boardTest
+{
+    class Command : ICommand
+    {
+        Action<object> action;
+
+        Func<object, bool> func;
+
+        public Command(Action<object> action,Func<object, bool> func)
+        {
+            this.action = action;
+            this.func = func;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return func(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            action(parameter);
+        }
+    }
+}
