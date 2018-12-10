acd34c2fa42936bd228bea69b61dd15183690511
ErikaHan <thshekkm4@gmail.com>
2018-12-10 23:02:49 +0900

    Vo추가

---
+++ HairSalonManager/Model/Vo/ReservedServiceVo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HairSalonManager.Model.Vo
+{
+    class ReservedServiceVo
+    {
+        public int ResNum { get; set; }
+
+        public int SerId { get; set; }
+    }
+}
---
+++ HairSalonManager/Model/Vo/UserVo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HairSalonManager.Model.Vo
+{
+    class UserVo
+    {
+        public string UserTel { get; set; }
+
+        public int Point { get; set; }
+    }
+}
