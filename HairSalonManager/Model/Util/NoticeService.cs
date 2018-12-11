77a87ef813f80366d70f7e9712781223bf457b7b
LAPTOP-5B9CGFQ3\soo98 <https://github.com/hansupark/phs123.github.io.git>
2018-12-11 23:15:18 +0900

    notice not yet

---
+++ HairSalonManager/Model/Util/NoticeService.cs
@@ -0,0 +1,22 @@
+﻿using HairSalonManager.Model.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HairSalonManager.Model.Util
+{
+    class NoticeService //예약 알림을 띄우는 클래스
+    {
+        private Connection _conn = Connection.Conn;
+
+        private static int _recentResNum;
+
+        public NoticeService()
+        {
+            //_recentResNum = 
+        }
+
+    }
+}
