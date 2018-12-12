e3f4914d818f3d919b4f00e05a0fabd0113b5877
LAPTOP-5B9CGFQ3\soo98 <https://github.com/hansupark/phs123.github.io.git>
2018-12-12 20:16:06 +0900

    reservation reposittory insert update delete

--- HairSalonManager/Model/Repository/ReservationRepository.cs
+++ HairSalonManager/Model/Repository/ReservationRepository.cs
@@ -11,6 +11,7 @@ namespace HairSalonManager.Model.Repository
 {
     class ReservationRepository
     {
+        #region singleTon 
         private static ReservationRepository _rr;
 
         public static ReservationRepository Rr
@@ -29,7 +30,9 @@ namespace HairSalonManager.Model.Repository
             _conn = Connection.Conn;
             RecentResNum = 0;
         }
+        #endregion
 
+        #region Fields
         private Connection _conn;
 
         private string _sql;
@@ -41,9 +44,14 @@ namespace HairSalonManager.Model.Repository
             {
                 return _recentResNum;
             }
-            private set;
+            private set
+            {
+                _recentResNum = value;
+            }
         }
+        #endregion
 
+        #region Reservation Methods
         public List<ReservationVo> GetReservations() //전체의 예약리스트를 가져옴.
         {           
             List<ReservationVo> list = new List<ReservationVo>();
@@ -98,5 +106,70 @@ namespace HairSalonManager.Model.Repository
             return list;
         }
 
+        public bool InsertReservation(ReservationVo rv) //예약 추가
+        {
+            _conn.Msc.Open();
+            _sql = "INSERT INTO reservation(resNum,stylistId,note,gender,userBirthday,startAt,endAt,userName) VALUES(@resNum,@resNum,@stylistId,@note,@gender,@userBirthday,@startAt,@endAt,@userName)";
+
+            MySqlCommand cmd = new MySqlCommand(_sql, _conn.Msc);
+
+            cmd.Parameters.Add("@resNum", rv.ResNum);
+            cmd.Parameters.Add("@stylistId", rv.StylistId);
+            cmd.Parameters.Add("@userTel", rv.UserTel);
+            cmd.Parameters.Add("@note", rv.Note);
+            cmd.Parameters.Add("@gender", rv.Gender);
+            cmd.Parameters.Add("@userBirthday", rv.UserBirthday);
+            cmd.Parameters.Add("@startAt", rv.StartAt);
+            cmd.Parameters.Add("@endAt", rv.EndAt);
+            cmd.Parameters.Add("@userName", rv.UserName);
+
+            if (cmd.ExecuteNonQuery() == -1) //실패시
+            {
+                return false;
+            }
+
+            return true; //성공시
+        }
+
+        public bool UpdateReservation(ReservationVo rv) //예약 수정
+        {
+            _conn.Msc.Open();
+            _sql = "UPDATE reservation SET stylistId = @stylistId, userTel = @userTel, " +
+                "note = @note, gender = @gender, userBirthday = @userBirthday, startAt = @startAt, endAt = @endAt, userName = @userName WHERE resNum = @resNum";
+            
+            MySqlCommand cmd = new MySqlCommand(_sql, _conn.Msc);
+
+            cmd.Parameters.Add("@resNum", rv.ResNum);
+            cmd.Parameters.Add("@stylistId", rv.StylistId);
+            cmd.Parameters.Add("@userTel", rv.UserTel);
+            cmd.Parameters.Add("@note", rv.Note);
+            cmd.Parameters.Add("@gender", rv.Gender);
+            cmd.Parameters.Add("@userBirthday", rv.UserBirthday);
+            cmd.Parameters.Add("@startAt", rv.StartAt);
+            cmd.Parameters.Add("@endAt", rv.EndAt);
+            cmd.Parameters.Add("@userName", rv.UserName);
+
+            if (cmd.ExecuteNonQuery() == -1) //실패시
+            {
+                return false;
+            }
+
+            return true; //성공시
+        }
+
+        public bool RemoveReservation(int resNum) //예약 삭제
+        {
+            _conn.Msc.Open();
+            _sql = $"DELETE FROM reservation WHERE resNum = {resNum}";
+            MySqlCommand cmd = new MySqlCommand(_sql, _conn.Msc);
+
+            if (cmd.ExecuteNonQuery() == -1) //실패시
+            {
+                return false;
+            }
+
+            return true; //성공시
+        }
+        #endregion 
     }
 }
