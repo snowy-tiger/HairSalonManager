051470b4c02fc4fe503331dbb23a326273482de5
HyeonBeom <hyunbum75@gmail.com>
2018-12-15 00:49:27 +0900

    Add GenderFunction

---
+++ HairSalonManager/Model/Util/GenderRadioConverter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace HairSalonManager.Model.Util
+{
+    class GenderRadioConverter : IValueConverter
+    {
+        //Data -> Converter -> View
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            //0 = 남자, 1 = 여자
+            int genderValue = (int)value;
+            int radioGender = Int32.Parse(parameter as string);
+
+            return genderValue == radioGender;
+        }
+
+        //View -> Converter -> Data
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            //0 = 남자, 1 = 여자
+            bool genderValue = (bool)value;
+            int radioGender = Int32.Parse(parameter as string);
+
+            if (!genderValue)
+                return Binding.DoNothing;
+            else
+                return radioGender;
+
+        }
+    }
+}
