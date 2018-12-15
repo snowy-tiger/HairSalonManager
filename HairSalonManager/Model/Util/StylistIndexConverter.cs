96706434ddb7750a5660ab616a54830256088b45
HyeonBeom <hyunbum75@gmail.com>
2018-12-15 17:40:45 +0900

    Complete StylistIndexConverter

--- HairSalonManager/Model/Util/StylistIndexConverter.cs
+++ HairSalonManager/Model/Util/StylistIndexConverter.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using HairSalonManager.Model.Repository;
+using HairSalonManager.Model.Vo;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Globalization;
 using System.Linq;
 using System.Text;
@@ -10,14 +13,24 @@ namespace HairSalonManager.Model.Util
 {
     class StylistIndexConverter : IValueConverter
     {
+        private ObservableCollection<StylistVo> _stylistList = new ObservableCollection<StylistVo>(StylistRepository.SR.GetStylistsFromLocal());
+
+        //Data -> Converter -> View
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            throw new NotImplementedException();
-        }
+            if (value == null)
+                return null;
+            uint stylistId = (uint)value;
+            return _stylistList.IndexOf(_stylistList.Single(x => x.StylistId == stylistId));
 
+        }
+        //View -> Converter -> Data
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            throw new NotImplementedException();
+            if (value == null)
+                return null;
+            int selectedIndex = (int)value;
+            return _stylistList[selectedIndex].StylistId;
         }
     }
 }
