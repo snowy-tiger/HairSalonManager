ebe882261d3c8f80ea7c1917fdb2153d74914de3
HyeonBeom <hyunbum75@gmail.com>
2018-12-19 02:40:49 +0900

    Bug Fix

--- HairSalonManager/Model/Util/TimeDivideConverter.cs
+++ HairSalonManager/Model/Util/TimeDivideConverter.cs
@@ -4,6 +4,7 @@ using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Data;
 
 namespace HairSalonManager.Model.Util
@@ -14,7 +15,7 @@ namespace HairSalonManager.Model.Util
         private List<int> _oldTimes;
         public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
         {
-            if (values[0]==null)
+            if (values[0]==null|| values[0] == DependencyProperty.UnsetValue)
                 return Binding.DoNothing;
             if (values[1] == null)
                 return Binding.DoNothing;
@@ -29,16 +30,16 @@ namespace HairSalonManager.Model.Util
         public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
         {
             string info = parameter as string;
-            int index = (int)value;
+            int selNumber = (int)value;
             
 
             switch (parameter)
             {
                 case "Hour":
-                    _oldValue = new DateTime(_oldValue.Year, _oldValue.Month, _oldValue.Day, _oldTimes[index], _oldValue.Minute, 0);
+                    _oldValue = new DateTime(_oldValue.Year, _oldValue.Month, _oldValue.Day, selNumber, _oldValue.Minute, 0);
                     break;
                 case "Minute":
-                    _oldValue = new DateTime(_oldValue.Year, _oldValue.Month, _oldValue.Day, _oldValue.Hour, _oldTimes[index], 0);
+                    _oldValue = new DateTime(_oldValue.Year, _oldValue.Month, _oldValue.Day, _oldValue.Hour, selNumber, 0);
                     break;
             }
 
