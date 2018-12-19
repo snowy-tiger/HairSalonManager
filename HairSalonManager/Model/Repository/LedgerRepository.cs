7996c08293cf88fb11f7c7734ad99872148af120
LAPTOP-5B9CGFQ3\soo98 <https://github.com/hansupark/phs123.github.io.git>
2018-12-19 20:50:26 +0900

    pay

---
+++ HairSalonManager/Model/Repository/LedgerRepository.cs
@@ -0,0 +1,60 @@
+﻿using HairSalonManager.Model.Vo;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HairSalonManager.Model.Repository
+{
+    class LedgerRepository : BaseRepository
+    {
+        #region singleTon
+        private LedgerRepository _ledgerRepository;
+
+        public LedgerRepository LR
+        {
+            get
+            {
+                if (_ledgerRepository == null)
+                {
+                    _ledgerRepository = new LedgerRepository();
+                }
+                return _ledgerRepository;
+            }
+        }
+        #endregion
+
+        #region ctor
+        private LedgerRepository()
+        {
+
+        }
+        #endregion
+
+        public uint InsertLedger(LedgerVo lv)
+        {
+            _conn.Msc.Open();
+
+            _sql = "INSERT INTO ledger(ledgerNum,resNum,sum) VALUES(@ledgerNum,@resNum,@sum)";
+
+            MySqlCommand cmd = new MySqlCommand(_sql, _conn.Msc);
+
+            cmd.Parameters.AddWithValue("@ledgerNum", lv.LedgerNum);
+            cmd.Parameters.AddWithValue("@resNum", lv.ResNum);
+            cmd.Parameters.AddWithValue("@sum", lv.Sum);
+
+            if (cmd.ExecuteNonQuery() == -1) //실패시
+            {
+                _conn.Msc.Close();
+                return 0;
+            }
+
+            uint recentId = (uint)cmd.LastInsertedId;
+            _conn.Msc.Close();
+            return recentId; //성공시
+           
+        }
+    }
+}
---
+++ HairSalonManager/Model/Repository/UserRepository.cs
@@ -0,0 +1,82 @@
+﻿using HairSalonManager.Model.Vo;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HairSalonManager.Model.Repository
+{
+    class UserRepository : BaseRepository
+    {
+        #region singleTon
+        private UserRepository _userRepository;
+
+        public UserRepository UR
+        {
+            get {
+                if (_userRepository == null)
+                    _userRepository = new UserRepository();
+                return _userRepository; }          
+        }
+        #endregion
+
+        #region ctor
+        private UserRepository()
+        {
+            _ds = new DataSet();
+            _sql = "SELECT * FROM user";
+            MySqlDataAdapter adapter = new MySqlDataAdapter(_sql,_conn.Msc);
+            adapter.Fill(_ds, "user");
+        }
+        #endregion
+
+        #region field
+        DataSet _ds;
+
+        #endregion
+
+        #region method
+        public bool InsertUser(UserVo user)
+        {
+            MySqlDataAdapter adapter = new MySqlDataAdapter(_sql, _conn.Msc);
+
+            DataTable table = _ds.Tables[0];
+            DataRow row = table.NewRow();
+
+            row["userTel"] = user.UserTel;
+            row["point"] = user.Point;
+ 
+            table.Rows.Add(row);
+
+            if (Save(adapter) == -1)
+                return false;
+            return true;
+        }
+
+        public bool UpdateUser(UserVo user)
+        {
+            MySqlDataAdapter adapter = new MySqlDataAdapter(_sql, _conn.Msc);
+
+            DataTable table = _ds.Tables[0];
+
+            DataRow row = table.Select().Single(x => (string)x["userTel"] == user.UserTel);
+            
+            row["point"] = user.Point;            
+
+            if (Save(adapter) == -1)
+                return false;
+            return true;
+        }
+
+        public int Save(MySqlDataAdapter adapter)
+        {
+            MySqlCommandBuilder builder = new MySqlCommandBuilder(adapter);
+            adapter.Update(_ds, "user");            
+            return 0;
+        }
+        #endregion
+    }
+}
