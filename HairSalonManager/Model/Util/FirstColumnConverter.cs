5d30f936e52c7770cb5331920214a4ef30e668c8
HyeonBeom <hyunbum75@gmail.com>
2018-12-20 01:38:18 +0900

    EditMainPage Design

---
+++ HairSalonManager/Model/Util/FirstColumnConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace HairSalonManager.Model.Util
+{
+    class FirstColumnConverter : IValueConverter
+    {
+        private int _count = 0;
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (_count > 48)
+                _count = 0;
+            if (_count++ == 0)
+                return 15;
+            else
+                return Binding.DoNothing;
+        }
+
+        //NOT IMPLEMENT
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
