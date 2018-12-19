b348c96954a99d643cd97e9d8ec2df212299acc9
LAPTOP-5B9CGFQ3\soo98 <https://github.com/hansupark/phs123.github.io.git>
2018-12-19 22:37:15 +0900

    setting pay

--- HairSalonManager/ViewModel/SettingViewModel.cs
+++ HairSalonManager/ViewModel/SettingViewModel.cs
@@ -1,81 +1,95 @@
-﻿//using HairSalonManager.Model.Vo;
-//using System;
-//using System.Collections.Generic;
-//using System.Collections.ObjectModel;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿using HairSalonManager.Model.Repository;
+using HairSalonManager.Model.Vo;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-//namespace HairSalonManager.ViewModel
-//{
-//    class SettingViewModel : ViewModelBase
-//    {
-//        #region field
-//        readonly SettingRepository _settingRepository;
-//        #endregion
+namespace HairSalonManager.ViewModel
+{
+    class SettingViewModel : ViewModelBase
+    {
+        #region field
+        readonly SettingRepository _settingRepository;
+        #endregion
 
+        #region property
+        private ObservableCollection<SettingVo> _settingList;
 
-//        #region property
-//        private ObservableCollection<SettingVo> _settingList;
+        public ObservableCollection<SettingVo> SettingList
+        {
+            get { return _settingList; }
+            set
+            {
+                _settingList = value;
+                OnPropertyChanged("SettingList");
+            }
+        }
 
-//        public ObservableCollection<SettingVo> SettingList
-//        {
-//            get { return _settingList; }
-//            set {
-//                _settingList = value;
-//                OnPropertyChanged("SettingList");
-//            }
-//        }
+        private SettingVo _selectedSetting;
 
-//        private SettingVo _selectedSetting;
+        public SettingVo SelectedSetting
+        {
+            get { return _selectedSetting; }
+            set
+            {
+                _selectedSetting = value;
+                OnPropertyChanged("SelectedSetting");
+            }
+        }
+        private string _search;
 
-//        public SettingVo SelectedSetting
-//        {
-//            get { return _selectedSetting; }
-//            set {
-//                _selectedSetting = value;
-//                OnPropertyChanged("SelectedSetting");
-//            }
-//        }
-
-//        public Command ModifyCommand { get; set; }
-//        public Command InitalizeCommand { get; }
-//        #endregion
-
-
-//        #region ctor
-//        public SettingViewModel()
-//        {
-//            _settingRepository = _settingRepository.SR;
-
-//            _selectedSetting = new SettingVo();
-
-//            SettingList = new ObservableCollection<SettingVo>(_settingRepository.GetSetting());
-
-//            ModifyCommand = new Command(ExecuteModifyMethod, CanExecuteMethod);
-//            InitalizeCommand = new Command(ExecuteInitalizeMethod, CanExecuteMethod);
-//        }
-
+        public string Search
+        {
+            get { return _search; }
+            set { _search = value;
+                OnPropertyChanged("Search");
+                OnSearchChanged();
+            }
+        }
         
-//        }
-//        #endregion
+
+        public Command ModifyCommand { get; set; }
+        public Command InitalizeCommand { get; }
+        
+        #endregion
 
 
-//        #region method
-//        private void ExecuteInitalizeMethod(object obj)
-//        {
-//            throw new NotImplementedException();
-//        }
+        #region ctor
+        public SettingViewModel()
+        {
+            _settingRepository = SettingRepository.SR;
 
-//        private bool CanExecuteMethod(object arg)
-//        {
-//            throw new NotImplementedException();
-//        }
+            _selectedSetting = new SettingVo();
 
-//        private void ExecuteModifyMethod(object obj)
-//        {
-//            throw new NotImplementedException();
-//        #endregion
+            SettingList = new ObservableCollection<SettingVo>(_settingRepository.GetSettings());
 
-//    }
-//}
+            ModifyCommand = new Command(ExecuteModifyMethod);
+            InitalizeCommand = new Command(ExecuteInitalizeMethod);
+        }
+
+
+
+        #endregion
+
+
+        #region method
+        private void ExecuteInitalizeMethod(object obj)
+        {
+            throw new NotImplementedException();
+        }     
+
+        private void ExecuteModifyMethod(object obj)
+        {
+            _settingRepository.UpdateSetting(SelectedSetting);
+        }
+
+        private void OnSearchChanged()
+        {
+            SettingList = new ObservableCollection<SettingVo>(_settingRepository.GetSettings().Where(x => x.Property == Search));
+        }
+        #endregion
+    }
+}
